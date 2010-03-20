bc8c135ef4f0f02f7e8ac009f4dad4ad057664fd
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-03-20 15:40:10 +0100

    Repair gp helper + gp specific auth

--- app/controllers/include/Helpers.rb
+++ app/controllers/include/Helpers.rb
@@ -1,106 +1,114 @@
 module Helpers
-    ### ADMIN STUFF ###
-    def is_admin
-	/gpadm/.match($0) != nil
+
+    ### AUTHENTICATION ###
+
+    def check_authentication
+        user=User.first({:nickname => @request[:l]})
+        if user.nil?
+            raise GridError, "Authentication failed, user with login #{@request[:l]} does not exist"
+        end
+        if(@request[:p] != Account.first({:user => user}).password) then
+            raise GridError, "Authentication failed, please check username and password"
+        end
     end
 
     ### ACCOUNT STUFF ###
 
     def get_account(id=:account_id)
-	account_id = @request[id]
+        account_id = @request[id]
         if(account_id.nil?) then
-		raise GridError, "No user provided"
-	end
+            raise GridError, "No user provided"
+        end
         user = User.first({:nickname => account_id})
         if(user.nil?) then
-		raise GridError, "User #{account_id} does not exist"
-	end
-	account = user.account
-	if(account.nil?) then
-		raise GridError, "No account linked to user #{user_id}"
-	end
-	return account
+            raise GridError, "User #{account_id} does not exist"
+        end
+        account = user.account
+        if(account.nil?) then
+            raise GridError, "No account linked to user #{user_id}"
+        end
+        return account
     end
 
     ### USER STUFF ###
 
     def get_user(id=:user_id)
-	user_id = @request[id]
+        user_id = @request[id]
         if(user_id.nil?) then
-		raise GridError, "No user provided"
-	end
+            raise GridError, "No user provided"
+        end
         user = User.first({:nickname => user_id})
         if(user.nil?) then
-		raise GridError, "User #{user_id} does not exist"
-	end
-	return user
+            raise GridError, "User #{user_id} does not exist"
+        end
+        return user
     end
 
     ### SENSOR STUFF ###
 
     def get_sensor(id=:sensor_id)
-	sensor_id = @request[id]
+        sensor_id = @request[id]
         if(sensor_id.nil?) then
-		raise GridError, "No sensor provided"
-	end
-	sensor = Sensor.first({:sensor_hr => sensor_id})
-	if(sensor.nil?) then
-		raise GridError,"Sensor #{sensor_id} does not exist"
-	end
-	return sensor
+            raise GridError, "No sensor provided"
+        end
+        sensor = Sensor.first({:sensor_hr => sensor_id})
+        if(sensor.nil?) then
+            raise GridError,"Sensor #{sensor_id} does not exist"
+        end
+        return sensor
     end
 
     ### MEASURE STUFF ###
 
     def get_measure(id=:measure_id)
-	measure_id = @request[id]
+        measure_id = @request[id]
         if(measure_id.nil?) then
-		raise GridError, "No measure provided"
-	end
-	measure = Measure.first({:measure_hr => measure_id})
-	if(measure.nil?) then
-		raise GridError,"Measure #{measure_id} does not exist"
-	end
-	return measure
+            raise GridError, "No measure provided"
+        end
+        measure = Measure.first({:measure_hr => measure_id})
+        if(measure.nil?) then
+            raise GridError,"Measure #{measure_id} does not exist"
+        end
+        return measure
     end
 
     ### OWNERSHIP ###
 
     def check_ownership_user_account(user,account)
-	if account.user_id = user
-		raise GridError, "Account is not linked to user #{user.nickname}"
-	end
+        if account.user_id = user
+            raise GridError, "Account is not linked to user #{user.nickname}"
+        end
     end
 
     def check_ownership_user_sensor(user,sensor)
-	if sensor.user != user then
-		raise GridError, "Sensor #{sensor.sensor_hr} does not belong to User #{user.nickname}"
-	end
+        if sensor.user != user then
+            raise GridError, "Sensor #{sensor.sensor_hr} does not belong to User #{user.nickname}"
+        end
     end
 
     def check_ownership_sensor_measure(sensor,measure)
-	if measure.sensor != sensor then
-		raise GridError, "Measure #{measure.measure_hr} does not belong to Sensor #{sensor.sensor_hr}"
-	end
+        if measure.sensor != sensor then
+            raise GridError, "Measure #{measure.measure_hr} does not belong to Sensor #{sensor.sensor_hr}"
+        end
     end
 
     ### ONLY USED IN USER PART ###
 
     def check_ownership_requestor_user(requestor,user)
-	if requestor.nickname != user.nickname
-		raise GridError, "Requestor #{requestor.nickname} and user requested #{user.nickname} do not match"
-	end
+        if requestor.nickname != user.nickname
+            raise GridError, "Requestor #{requestor.nickname} and user requested #{user.nickname} do not match"
+        end
     end
 
 
     ### UTIL ###
 
     def clean_id(hash)
-	hash.delete(:id)
-	hash.delete(:user_id)
-	hash.delete(:account_id)
-	hash.delete(:sensor_id)
-	hash
+        hash.delete(:id)
+        hash.delete(:user_id)
+        hash.delete(:account_id)
+        hash.delete(:sensor_id)
+        hash
     end
 
     ### STAT PURPOSES ###
@@ -109,6 +117,6 @@ module Helpers
         days_nbr = 1
         days_nbr = @request[:days_nbr] if not @request[:days_nbr].nil?
         start_date = DateTime.now - days_nbr.to_i
-	return start_date
+        return start_date
     end
 end
