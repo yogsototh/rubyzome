6a2d2af3efe876ffce8ef20f52cdbc52fab48b1c
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-03-22 15:18:46 +0100

    Dispatcher insinde Rubyzome framework

--- rubyzome/rubyzome.rb
+++ rubyzome/rubyzome.rb
@@ -1,15 +1,19 @@
 # encoding: utf-8
 
+# Rubyzome central file which load all submodules
+# and subclasses
 module Rubyzome
-
     require 'dm-core'
 
     # Include all rubyzome classes
-    require 'rubyzome/lib'
-    require 'rubyzome/controllers'
-    require 'rubyzome/views'
-    require 'rubyzome/models'
+    require 'rubyzome/lib.rb'
+    require 'rubyzome/controllers.rb'
+    require 'rubyzome/views.rb'
+    require 'rubyzome/models.rb'
 
     # load the classes for the defined application
-    require 'rubyzome/load_local_app'
+    require 'rubyzome/load_local_app.rb'
+
+    # load the class that will handle server requests
+    require 'rubyzome/RestfulDispatcher.rb'
 end
