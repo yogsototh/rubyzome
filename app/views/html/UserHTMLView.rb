f2f6236a7a7418b1fd1c10f8165ff26f37fe4f7b
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-03-31 22:21:15 +0200

    Better view for user and message.

--- app/views/html/UserHTMLView.rb
+++ app/views/html/UserHTMLView.rb
@@ -2,14 +2,11 @@
 
 class UserHTMLView < Rubyzome::HTMLView
     def content(object)
-        if object.class == Hash and object.has_key?(:html_content)
-            return super
-        else
-            @title=@request[:nickname]
-            @subtitle="Your public informations"
-            @content=%{Your nickname is <strong>#{@request[:user_id]}</strong><br/>Anything else is private.}
-        end
+        @title=object[:nickname]
+        @subtitle=object[:status]
+        @content=%{Welcome <em>#{object[:nickname]}</em>.<p>Your last hour consumption:</p>
+        <div id="graph">Graphique</div>}
         render
     end
 end
-UserHTMLView.template=File.read('app/views/html/templates/second.erb')
+UserHTMLView.template=File.read('app/views/html/templates/user.erb')
