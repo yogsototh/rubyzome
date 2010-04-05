21e7c8bde7c802e433be11f4608af646abc2c521
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-04-05 21:52:31 +0200

    User view should be identical between gp and gpadm

--- app/views/html/UserHTMLView.rb
+++ app/views/html/UserHTMLView.rb
@@ -1,15 +1,18 @@
+# encoding: utf-8
 
-
-class UserHTMLView < HTMLView
+class UserHTMLView < Rubyzome::HTMLView
     def content(object)
-        if object.class == Hash and object.has_key?(:html_content)
-            return super
+        if object[:html_title]
+            @title="Authentification error"
+            @subtitle="500"
+            @content=%{Please try a new password.
+                <script>top.location = "/";</script>}
         else
-            @title=@request[:nickname]
-            @subtitle="Your public informations"
-            @content=%{Your nickname is <strong>#{@request[:user_id]}</strong><br/>Anything else is private.}
+            @title=object[:nickname]
+            @subtitle=object[:status]
+            @content=%{Welcome <em>#{object[:nickname]}</em>.<p>Your last hour consumption:</p><div id="graph"></div>}
         end
         render
     end
 end
-UserHTMLView.template=File.read('app/views/html/templates/second.erb')
+UserHTMLView.template=File.read('app/views/html/templates/user.erb')
