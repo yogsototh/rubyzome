6d478b6112e7425dcc2174cf3ad17378704a4952
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-04-06 02:43:39 +0200

    new account view

--- app/views/html/AccountHTMLView.rb
+++ app/views/html/AccountHTMLView.rb
@@ -1,7 +1,18 @@
 # encoding: utf-8
-
 class AccountHTMLView < Rubyzome::HTMLView
     def content(object)
+        if object[:html_title]
+            @title="Authentification error"
+            @subtitle="500"
+            @content=%{Please try a new password.
+                <script>top.location = "/";</script>}
+        else
+            @object=object
+            @title="Account Setting"
+            @subtitle=object[:nickname]
+            @content=%{}
+        end
         render
     end
 end
+AccountHTMLView.template=File.read('app/views/html/templates/account.erb')
