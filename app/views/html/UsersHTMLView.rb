f8d97e2947280605b718aa02e759b2f1d2ed7c33
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-04-07 13:32:10 +0200

    Return if error

--- app/views/html/UsersHTMLView.rb
+++ app/views/html/UsersHTMLView.rb
@@ -4,8 +4,15 @@ fatherClass=Rubyzome::HTMLView.dup
 
 class UsersHTMLView < fatherClass
     def content(object)
-        @title="Users"
-        @object=object
+        if object.is_a?(Hash) and object[:html_title]
+            init_titles_from(object)
+            @request={ :l => "undefined", :p => "undefined" }
+            @object={ :nickname => "undefined" }
+            @content<<= %{<p>Wait to return to login page</p><script>setTimeout(function(){window.location="/";},3000);</script>}
+        else
+            @title="Users"
+            @object=object
+        end
         render
     end
 end
