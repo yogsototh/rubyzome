1677367128747963ce72df0124f275dffc0bb032
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-05-03 16:19:43 +0200

    Adaption to error view

--- app/views/html/TodolistHTMLView.rb
+++ app/views/html/TodolistHTMLView.rb
@@ -2,18 +2,12 @@
 
 class TodolistHTMLView < Rubyzome::HTMLView
     def content(object)
-        if object[:html_title]
-            init_titles_from(object)
-            @request={ :l => "undefined", :p => "undefined" }
-            @object={ :nickname => "undefined" }
-            @content<<= %{<p>Wait to return to login page</p><script>setTimeout(function(){window.location="/";},3000);</script>}
-        else
-            @object=object
-            @title=object[:title]
-            @subtitle=object[:uid]
-            @content=%{}
-        end
+        @object=object
+        @title=object[:title]
+        @subtitle=object[:uid]
+        @content=%{}
         render
     end
 end
 TodolistHTMLView.template=File.read('app/views/html/templates/todolist.erb')
+TodolistHTMLView.error_template=File.read('rubyzome/views/html/templates/error.erb')
