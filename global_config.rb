642696cba3496fe4305ca327eab32136dc012d14
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-05-24 23:15:56 +0200

    sqlite3 configuration

--- global_config.rb
+++ global_config.rb
@@ -5,11 +5,11 @@
 # a REST resource of the application
 $directory_of_website='/website'
 
-# db configuration
-$db_type='mysql'
-$db_user='todo'
-$db_password='todo'
-$db_admin_user='todoadmin'
-$db_admin_password='todoadmin'
-$db_host='localhost'
-$db_database='todo'
+# SQLite3 conf
+$db_type="sqlite3"
+$db_user=nil
+$db_password=nil
+$db_admin_user=nil
+$db_admin_password=nil
+$db_host=nil
+$db_database='/tmp/db.sqlite3'
