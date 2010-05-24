16c60fc9e14452999d7c1c47f56d2d8b4a5e70b8
Luc Juggery <luc.juggery@gmail.com>
2010-05-24 23:13:57 +0200

    Add config for sqlite3

--- global_config.rb
+++ global_config.rb
@@ -6,10 +6,19 @@
 $directory_of_website='/website'
 
 # db configuration
-$db_type='mysql'
-$db_user='todo'
-$db_password='todo'
-$db_admin_user='todoadmin'
-$db_admin_password='todoadmin'
-$db_host='localhost'
-$db_database='todo'
+# $db_type='mysql'
+# $db_user='todo'
+# $db_password='todo'
+# $db_admin_user='todoadmin'
+# $db_admin_password='todoadmin'
+# $db_host='localhost'
+# $db_database='todo'
+
+# SQLite3 conf
+$db_type="sqlite3"
+$db_user=nil
+$db_password=nil
+$db_admin_user=nil
+$db_admin_password=nil
+$db_host=nil
+$db_database='/Users/luc/Projects/todo.sqlite3'
