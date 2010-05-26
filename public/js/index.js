5eb2ee2820de3bf45c8dd3e8026a97fbdbf1bd3e
Luc Juggery <luc.juggery@gmail.com>
2010-05-26 19:33:48 +0200

    Add todo creation Problem while retrieving todos of each list

--- public/js/index.js
+++ public/js/index.js
@@ -13,8 +13,7 @@ $(document).ready(function(){
     setSpecificCss();
 
     // Display list of existing todolists within lists div
-    $.getJSON("/todolists.json",
-        function(data){
+    $.getJSON("/todolists.json", function(data){
             $.each(data, function(i,item){
 		list = $('<div id="' + item.id + '" class="list">' + item.title + '</div>');
 		$('#lists').append(list);
@@ -27,17 +26,36 @@ $(document).ready(function(){
                 });
                 */
 
+    // Get list of todos for the current list
+    // TODO
+/*
+    $.getJSON("/todos.json", function(td){
+        alert(1);
+        alert(td);
+        $.each(td, function(i,tdi){
+            tdo = $('<div id="' + tdi.id + '" class="todo">' + tdi.description + '</div>');
+            $('#lists').append(tdo);
+        });
+    });
+*/
 		// Add text field for new todo
 	       todo = $('<form id="create_new_todo' + item.id + '"><input name="todo" type="text" id="todo_' + item.id + '" class="todo" value="New todo"/></form>');
 		$('#' + item.id).append(todo);
 
 		// Add event handle on todo
 		$('#create_new_todo' + item.id).submit(function() {
-			alert("About to add a new todo to the list");
-		});
-            });
-        }
-    );
+		    alert("About to add a new todo to the list");
+		    create("/todos",
+                        {description: $('input[id="todo_' + item.id + '"]').val(), todolist_id: item.id},
+                        function(){alert('todolist created in DB')},
+                        function (xhr, ajaxOptions, thrownError){
+                            alert(xhr.status);
+                            alert(ajaxOptions);
+                            alert(thrownError);
+                        });
+                });
+	  });
+    });
 
     $('#newlist').click(function() {
 	$('#title').removeClass('inactive');
