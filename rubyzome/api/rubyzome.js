c1eed18c9b76ade9aa22134ad09d075304def2ae
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-05-31 15:22:03 +0200

    Update from tuto/todogether

---
+++ rubyzome/api/rubyzome.js
@@ -0,0 +1,28 @@
+// general action
+// f_ok(data, textStatus, XMLHttpRequest)
+// f_err(XMLHttpRequest,textStatus, errorThrown)
+function general_action( type, url, params, f_ok, f_err ) {
+    $.ajax({ 
+        type: type,
+        url:  url+'.json',
+        data: params,
+        success: f_ok, 
+        error:  f_err
+        });
+}
+
+function create(url, params, f_ok, f_err) {
+    return general_action( "POST", url, params, f_ok, f_err);
+}
+function list(url, params, f_ok, f_err) {
+    return general_action( "GET", url, params, f_ok, f_err);
+}
+function show(url, params, f_ok, f_err) {
+    return general_action( "GET", url, params, f_ok, f_err);
+}
+function update(url, params, f_ok, f_err) {
+    return general_action( "PUT", url, params, f_ok, f_err);
+}
+function rest_delete(url, params, f_ok, f_err) {
+    return general_action( "DELETE", url, params, f_ok, f_err);
+}
