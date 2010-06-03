3e1cea0f58a25e4541b51046842f23b97fc6fc68
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-06-03 16:30:58 +0200

    Better portability

--- global_config.rb
+++ global_config.rb
@@ -5,17 +5,6 @@
 # a REST resource of the application
 $directory_of_website='/website'
 
-# -- db configuration --
-
-#  MySQL conf
-# $db_type='mysql'
-# $db_user='todogether'
-# $db_password='18582fdee46ef66db2937cd40b90c9eb7bfe310a'
-# $db_host='localhost'
-# $db_database='todogether'
-# $db_admin_user='todoadmin'
-# $db_admin_password='18582fdee46ef66db2937cd40b90c9eb7bfe310a'
-
 # SQLite3 conf
 $db_type="sqlite3"
 $db_user=nil
@@ -23,4 +12,4 @@ $db_password=nil
 $db_admin_user=nil
 $db_admin_password=nil
 $db_host=nil
-$db_database='/Users/esposito/Sites/gp/db.sqlite3'
+$db_database='/tmp/db.sqlite3'
