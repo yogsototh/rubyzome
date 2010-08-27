0157ec0b2a6198b27de96615303479e769e2cd76
LJ <luc.juggery@gmail.com>
2010-08-27 16:14:27 +0200

    Change config stuff

--- rubyzome/rubyzome.rb
+++ rubyzome/rubyzome.rb
@@ -5,20 +5,32 @@ module Rubyzome
     require 'dm-core'
 
     # Include all rubyzome classes
-    Dir["rubyzome/classes/*.rb"].each { |file| puts file; require file }
+    Dir["rubyzome/classes/*.rb"].each { |file| require file }
     def self.const_missing(c)
         Object.const_get(c)
     end
 
+    # Load all rubyzome standard views
+    $views = {} unless defined? $views
+    Dir["rubyzome/classes/*View.rb"].each do |file| 
+        next if file == "/rubyzome/views/RestView.rb"
+        viewname=File.basename(file,File.extname(file))
+        $views[viewname]=Rubyzome.const_get(viewname)
+    end
+
     # Include all application specific classes
     Dir["app/models/*.rb"].each { |file| require file }
+    Dir["app/controllers/*.rb"].each { |file| require file }
+
+    # Load all application specific views
     Dir["app/views/*/*.rb"].each do |file| 
       require file 
       viewname=File.basename(file,File.extname(file))
       typename=File.basename(File.dirname(file))
        $views['/'+typename+'/'+viewname]=Kernel.const_get(viewname)
     end
-    Dir["app/controllers/*.rb"].each { |file| require file }
+
+    # Set Datamapper stuff
     DataMapper.setup(:default,$db_url)
     DataMapper.finalize
 end
