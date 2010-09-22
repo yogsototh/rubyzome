7a233f133d999ff5f277bde1f9eb4460f17b3889
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-09-22 11:50:39 +0200

    Fixed bug for fulljs website

--- public/static/js/grid.js
+++ public/static/js/grid.js
@@ -75,8 +75,8 @@ function showUserConsumption(){
 
 				// only for first sensor
 				sensor=json[0]["sensor_hr"];
-				var last_measure_param = { "l": user, "p" : password };
-				var last_day_measure_param = { "l": user, "p" : password, "from" : one_day_ago.toString(), "to": now.toString(), interval: 1800 };
+				var last_measure_param = { "l": user, "p" : password, "v": 2 };
+				var last_day_measure_param = { "l": user, "p" : password, "from" : one_day_ago.toString(), "v": 2, "to": now.toString(), interval: 1800 };
 				$.getJSON(prefix_url+'/'+sensor+'/measures.json', last_day_measure_param, function(measure) {
 					draw_graphic( measure );
 				});
