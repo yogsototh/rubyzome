d42d112520924273da7cd0e1bdf294ef6970c522
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-11-24 13:09:31 +0100

    Better merge

--- global.rb
+++ global.rb
@@ -7,4 +7,8 @@ $static_files_directory='/static'
 # the DB URL default is an sqlite db file: datas.db 
 # if the env variable DATABASE_URL is set it is the one choosen
 # With this it works seemlessly with heroku
-$db_url=ENV['DATABASE_URL'] || %{sqlite3://#{Dir.pwd}/datas.db}
+if ENV['GPENV'] == "PRODUCTION"
+    $db_url="f5lxx1zz5pnorynqglhzmsp34@ec2-184-72-239-7.compute-1.amazonaws.com"
+else
+    $db_url=ENV['DATABASE_URL'] || %{sqlite3://#{Dir.pwd}/datas.db}
+end
