086c3c71e2146b2a828d9e607a475f540d2636cd
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-11-24 13:28:00 +0100

    Added postgresql to the url of the DB

--- global.rb
+++ global.rb
@@ -8,7 +8,7 @@ $static_files_directory='/static'
 # if the env variable DATABASE_URL is set it is the one choosen
 # With this it works seemlessly with heroku
 if ENV['GPENV'] == "PRODUCTION"
-    $db_url="f5lxx1zz5pnorynqglhzmsp34@ec2-184-72-239-7.compute-1.amazonaws.com"
+    $db_url="postgresql://f5lxx1zz5pnorynqglhzmsp34@ec2-184-72-239-7.compute-1.amazonaws.com"
 else
     $db_url=ENV['DATABASE_URL'] || %{sqlite3://#{Dir.pwd}/datas.db}
 end
