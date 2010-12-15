88a4fb111028da93ad3e70c6c7d5731bcc0c499f
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-12-15 21:27:05 +0100

    Better login page (removed many bugs)

--- public/static/js/grid.js
+++ public/static/js/grid.js
@@ -13,6 +13,8 @@ function getUserFromCookie() {
 
 function logout() {
     $.cookie('user',null);
+    $.cookie('password',null);
+    $.cookie('remember',null);
     return true; // in order not to disable the link
 }
 
@@ -25,35 +27,43 @@ function select_and_active() {
 	$(this).removeClass('inactive')
 }
 
+function clearInput() {
+    this.value=''; 
+    this.select(); 
+    $(this).removeClass('inactive');
+}
+
+function setDefaultInput(elem, defaultValue) {
+    if (!elem.value) {
+        elem.value=defaultValue; 
+        $(elem).addClass('inactive');
+    }
+}
+
+
 // after document loaded
 $(document).ready(function(){ 
 	if ( getUserFromCookie() ) {
 		showUserConsumption();
     } else {
-	    $("#username").click(function() { $(this).select(); $(this).removeClass('inactive'); });
-	    $("#username").focus(function() { $(this).select(); $(this).removeClass('inactive'); });
-	    $("#password").click(function() { $(this).select(); $(this).removeClass('inactive'); });
-	    $("#password").focus(function() { $(this).select(); $(this).removeClass('inactive'); });
-
-	    $('#username').change(function(){
-	    	if ( $(this).val() == '' || $(this).val() == 'User Name') {
-	    		$(this).val('User Name');
-	    		$(this).addClass('inactive');
-	    	}
-	    });
-
-	    $('#password').change(function(){
-	    	if ( $(this).val() == '' ) {
-	    		$(this).val('password');
-	    		$(this).addClass('inactive');
-	    	}
-	    });
+	    $("#username").click(clearInput);
+	    $("#username").focus(clearInput);
+	    $("#username").blur( function() {setDefaultInput(this,"User Name");});
+	    $("#password").click(clearInput);
+	    $("#password").focus(clearInput)
 
 	    $('form[name=login_form]').submit(function (){
 	    	user = $('[name=l]').val();
 	    	password = $('[name=p]').val();
-            $.cookie('user',user);
-            $.cookie('password',password);
+            if ($('#remember').attr('checked')) {
+                $.cookie('user',user,{expires: 14});
+                $.cookie('password',password,{expires: 14});
+                $.cookie('remember',true,{expires: 14});
+            } else {
+                $.cookie('user',null);
+                $.cookie('password',null);
+                $.cookie('remember',true,null);
+            }
 	    	showUserConsumption();
 	    	return false;
             });
@@ -85,7 +95,7 @@ function showUserConsumption(){
 				// only for first sensor
 				sensor=json[0]["sensor_hr"];
 				var last_measure_param = { "l": user, "p" : password, "v": 2 };
-				var last_day_measure_param = { "l": user, "p" : password, "from" : one_day_ago.toString(), "v": 2, "to": now.toString(), interval: 300 };
+				var last_day_measure_param = { "l": user, "p" : password, "from" : last_midnight.toString(), "v": 2, "to": next_midnight.toString(), interval: 300 };
                 update_graphic(prefix_url, user, password, sensor, last_day_measure_param);
                 update_instant_consumption(prefix_url, user, password, sensor, last_measure_param);
 				showMenu();
@@ -222,7 +232,11 @@ function draw_graphic( data ) {
     var interval=data["interval"];
     $.each(data["data"],function(index, value) {
 	if (index) {
-	    datatabs[index]=[ from.getTime() + (index*interval*1000), value ];
+        if ( value != -1 ) {
+	        datatabs[index]=[ from.getTime() + (index*interval*1000), value ];
+        } else {
+	        datatabs[index]=[ from.getTime() + (index*interval*1000), null ];
+        }
 	}
     });
     $.plot($('#graph'), [ { color: "#CFF", 
@@ -243,5 +257,8 @@ function draw_graphic( data ) {
             });
 }
 
-var now=new Date
-var one_day_ago=new Date((new Date).getTime() - 24 * 3600000)
+var now=new Date;
+var one_day_ago=new Date((new Date).getTime() - 24 * 3600000);
+var preceeding_midnight=new Date(one_day_ago.getFullYear(), one_day_ago.getMonth(), one_day_ago.getDay(), 0, 0, 0, 0);
+var last_midnight=new Date(now.getFullYear(), now.getMonth(), now.getDay(), 0, 0, 0, 0);
+var next_midnight=new Date(now.getFullYear(), now.getMonth(), now.getDay(), 23, 59, 59, 999);
