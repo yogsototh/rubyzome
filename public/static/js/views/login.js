8fe9981ad891e4927c32f31f285ddc79cb12ba57
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-12-20 16:58:06 +0100

    Progress

--- public/static/js/views/login.js
+++ public/static/js/views/login.js
@@ -1,53 +1,57 @@
 var LoginView = function() {
-    var self=this;
-}
-
-LoginView.prototype.become_active = function() { 
-    $(this).removeClass('inactive'); 
-}
-LoginView.prototype.select_and_active = function select_and_active() {
-    $(this).select();
-    $(this).removeClass('inactive');
-}
-LoginView.prototype.clearInput = function () {
-    this.value=''; 
-    this.select(); 
-    $(this).removeClass('inactive');
-}
-
-LoginView.prototype.setDefaultInput = function (elem, defaultValue) {
-    if (!elem.value) {
-        elem.value=defaultValue; 
-        $(elem).addClass('inactive');
-    }
-}
-
-
-LoginView.prototype.htmlLoaded = function() {
-    $("#username").click(self.clearInput);
-    $("#username").focus(self.clearInput);
-    $("#username").blur( function() {self.setDefaultInput(this,"User Name");});
-    $("#password").click(self.clearInput);
-    $("#password").focus(self.clearInput);
-
-    $('form[name=login_form]').submit(function (){
-        mainApplication.setUser( $('[name=l]').val());
-        mainApplication.setPassword( $('[name=p]').val());
-        if ($('#remember').attr('checked')) {
-            $.cookie('user',mainApplication.user,{expires: 14});
-            $.cookie('password',mainApplication.password,{expires: 14});
-            $.cookie('remember',true,{expires: 14});
-        } else {
-            $.cookie('user',null);
-            $.cookie('password',null);
-            $.cookie('remember',true,null);
-        }
-        self.application.showUserConsumption();
-        return false;
-    });
+    this.self=this;
 }
 
 LoginView.prototype.show = function() {
+    var self=this;
     $('#content').load("/static/html/login.html",self.htmlLoaded);
 }
+
+LoginView.prototype.submitForm = function() {
+    mainApplication.setUser( $('[name=l]').val());
+    mainApplication.setPassword( $('[name=p]').val());
+    if ($('#remember').attr('checked')) {
+        $.cookie('user',mainApplication.user,{expires: 14});
+        $.cookie('password',mainApplication.password,{expires: 14});
+        $.cookie('remember',true,{expires: 14});
+    } else {
+        $.cookie('user',null);
+        $.cookie('password',null);
+        $.cookie('remember',true,null);
+    }
+    mainApplication.showUserConsumption();
+    return false;
+}
+
+LoginView.prototype.test = function () {
+    alert('Prototype test');
+}
+
+LoginView.prototype.htmlLoaded = function() {
+    var self=this;
+    console.log('htmlLoaded [typeof this='+typeof(this)+']');
+    console.log('[typeof this.test='+typeof(this.test)+']');
+    // self.autoclear('username','User Name');
+    // self.autoclear('password','');
+    /*
+    $("#username").click(
+            function() { 
+                this.value=''; 
+                this.select(); 
+                $(this).removeClass('inactive'); });
+    $("#username").focus(function() { 
+            this.value=''; 
+            this.select(); 
+            $(this).removeClass('inactive'); });
+    $("#username").blur(
+            function(){ if ( this.value == '' ) {
+                this.value='User Name'; $(this).addClass('inactive');}
+            });
+
+    $("#password").click(self.autoclear);
+    $("#password").focus(self.autoclear);
+    */
+    $('form[name=login_form]').submit(function(){self.submitForm()});
+}
+
 
