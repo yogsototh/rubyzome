1e0e0f8c35af54e4c4b521229700ee691dccc700
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-12-21 14:53:01 +0100

    Instant consumption subview OK

--- public/static/js/views/consumption.js
+++ public/static/js/views/consumption.js
@@ -1,7 +1,7 @@
 var ConsumptionView = function() {
     this.user = mainApplication.user;
     this.password = mainApplication.password;
-    this.login_params = { l: this.user, p: this.password };
+    this.login_params = { l: this.user, p: this.password, v:2 };
 }
 
 ConsumptionView.prototype.show = function(){
@@ -33,6 +33,28 @@ ConsumptionView.prototype.showInstantConsumptionSubview = function() {
 }
 
 ConsumptionView.prototype.getInstantConsumptionDatas = function() {
+    var self=this;
+    $.getJSON( '/users/'+self.user+'/sensors/'+self.sensor+'/measures.json',
+                self.login_params,
+                function(measure) {
+					var len=measure["data"].length;
+					var last=measure["data"][len-1];
+                    // var last_measure_date=new Date.parse( measure["date"] ) ;
+                    // if ( last_measure_date - now > max_time ) {
+                    //  no_instant_data();
+                    // }
+
+					var cons=last;
+					var KWH_COST=0.082;
+					var HOURLY_COST_MULTI = 0.001;
+					var DAILY_COST_MULTI = 0.024;
+					var MONTHLY_COST_MULTI = 0.720;
+					$('#instantconsumptionvalue').html( cons + ' Watts' );
+					$('#instanthourlycostvalue').html( (cons * KWH_COST * HOURLY_COST_MULTI).toFixed(2) + " €");
+					$('#instantdailycostvalue').html( (cons * KWH_COST * DAILY_COST_MULTI).toFixed(2) + " €");
+					$('#instantmonthlycostvalue').html( (cons * KWH_COST * MONTHLY_COST_MULTI).toFixed(2) + " €");
+                }
+                )
 }
 
 ConsumptionView.prototype.showLineChartSubview() = function() {
