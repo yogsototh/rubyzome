ef43e38469c6323789376c4811e7dd4a782cce9f
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-12-29 15:32:39 +0100

    Reseted DB config

--- global.rb
+++ global.rb
@@ -7,8 +7,4 @@ $static_files_directory='/static'
 # the DB URL default is an sqlite db file: datas.db 
 # if the env variable DATABASE_URL is set it is the one choosen
 # With this it works seemlessly with heroku
-if ENV['GPENV'] == "PREPROD"
-    $db_url="postgres://mnyqdiisby:f5lxx1zz5pnorynqglhzmsp34@ec2-174-129-199-187.compute-1.amazonaws.com/mnyqdiisby"
-else
-    $db_url=ENV['DATABASE_URL'] || %{sqlite3:///tmp/datas.db}
-end
+$db_url=ENV['DATABASE_URL'] || %{sqlite3:///tmp/datas.db}
