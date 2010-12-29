3d5857367b8b751a56ac6d7b8f85020935c9cc38
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-12-29 11:11:08 +0100

    Fixed some account issues.

--- public/static/js/views/account.js
+++ public/static/js/views/account.js
@@ -1,7 +1,7 @@
 var AccountView = function() {
     this.user = mainApplication.user;
     this.password = mainApplication.password;
-    this.message = "not loaded yet";
+    this.message = "";
     this.login_params = { l: this.user, p: this.password, v:2 };
     this.num = 0;
 }
@@ -22,26 +22,31 @@ AccountView.prototype.showUserAccount = function() {
 
     $('#nickname').html(self.user);
     $('#password').val(self.password);
-    $('#message').html(self.message);
+    $.getJSON('/users/'+self.user+'.json',
+            {l:self.user, p:self.password},
+            function(json) {
+                self.message = json["status"];
+                $('#message').html(self.message);
+            });
     $('#updateButton').click(function(){ self.update_resource(self)} );
 }
 
 AccountView.prototype.update_resource = function (self) {
 
-    mainApplication.log('self.user = '+self.user);
-    mainApplication.log('self.password = '+self.password);
-    mainApplication.log('self.message = '+self.message);
+    var newpassword=$('#password').val();
 
     $.post(
 	    '/accounts/'+self.user+'.json', 
 	    {   l: self.user,
 	        p: self.password,
 	        nickname: self.user, 
-	        password: $('#password').val(),
+	        password: newpassword,
 	        _method: 'PUT'
 	    },
 	    function() {
-            mainApplication.setPassword(self.password);
+            mainApplication.setPassword(newpassword);
+            self.password = mainApplication.password;
+            var num=self.num;
 	        $("#info").prepend('<div id="updated'+num+'">'+self.user+' updated!</div>');
 	        setTimeout(function(){$('#updated'+num).remove()},1500);
 	        self.num++;
