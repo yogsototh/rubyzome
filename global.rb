158ccef7ddcd3b8ce70e2311438d9f280b3be42b
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-12-29 15:44:37 +0100

    Added configuration to share preprod DB

--- global.rb
+++ global.rb
@@ -9,6 +9,10 @@ $static_files_directory='/static'
 # With this it works seemlessly with heroku
 if ENV['GPENV'] == "PRODUCTION"
     $db_url="postgres://mnyqdiisby:f5lxx1zz5pnorynqglhzmsp34@ec2-174-129-199-187.compute-1.amazonaws.com/mnyqdiisby"
+elsif ENV['GPENV'] == "PREPROD"
+    $db_url="postgres://xnerjchzzq:f5lxx1zz5pnorynqglhzmsp34@ec2-50-16-228-68.compute-1.amazonaws.com/xnerjchzzq"
+elsif ENV['GPENV'] == "Y"
+    $db_url="" # TODO
 else
     $db_url=ENV['DATABASE_URL'] || %{sqlite3:///tmp/datas.db}
 end
