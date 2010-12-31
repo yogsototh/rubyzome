ee6c58bdfbb536f76483bbdde9fbabc60bdaee9b
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2010-12-31 13:08:59 +0100

    Used coffeescript for a more readable code

---
+++ coffee/init.coffee
@@ -0,0 +1,161 @@
+class Application
+    user: ""
+    password: ""
+    remember: false
+
+    log: (msg) ->
+        console.log(msg) if console? and console.log?
+
+    save: (info,value) ->
+        this.log "save('#{info}','#{value}')"
+        $.cookie(info,value,{expires: 14})
+
+    forget: (info) ->
+        this.log 'forget("'+info+'")' 
+        $.cookie(info,null)
+
+    setRemember: (remember) -> 
+        if remember
+            this.save 'user',this.user
+            this.save 'password',this.password
+            this.save 'remember',true
+        else
+            this.forget 'user'
+            this.forget 'password'
+            this.forget 'remember'
+
+    setUser: (user) -> 
+        this.user = user
+        if this.remember
+            this.save 'user',this.user
+
+    setPassword: (password) ->
+        this.password = password
+        if this.remember
+            this.save 'password',this.password
+
+    retrieveSavedPreferences: ->
+        self=this
+        self.remember = $.cookie('remember')
+        self.user = $.cookie('user')
+        if self.user
+            self.password = $.cookie('password')
+            return true
+        return false
+
+    logout: ->
+        $.cookie('user',null)
+        $.cookie('password',null)
+        $.cookie('remember',null)
+        $.cookie('lastSelectedView',null)
+        return true; # in order not to disable the link
+
+    # execute the function action after all files are loaded only if needed
+    #
+    # example of usage:
+    #
+    # files=[]; 
+    # tests=[]
+    #
+    # files.push('/static/js/date.js')
+    # tests.push('Date.prototype.setISO8601')
+    #
+    # files.push('/static/js/flot/jquery.flot.js')
+    # tests.push('$.flot')
+    #
+    # mainApplication.run_after_dependencies( files, tests, 
+    #         function() {
+    #            $('#content').load("/static/html/user_stats.html",
+    #                function(){ self.htmlLoaded(self);})
+    #            })
+    #
+    #
+    run_after_dependencies: ( files, tests, action ) ->
+        self=this
+        if files.length==0
+            action()
+        else
+            file=files.pop()
+            test=tests.pop()
+            eval('o='+test+';')
+            if ( typeof o == "undefined")
+                $.getScript file, 
+                            ->
+                              self.run_after_dependencies(files, tests, action)
+            else
+                self.run_after_dependencies(files, tests, action)
+
+    connectionSuccessful: (self, success, failed ) -> 
+        $.ajax( url: 
+                    '/users/'+self.user+'.json'
+                data: 
+                     l: self.user
+                     p: self.password
+                success: 
+                    success
+                error: 
+                    failed
+                )
+
+    run: ->
+        self=this
+        if self.retrieveSavedPreferences()
+            self.connectionSuccessful(
+                self,
+                -> 
+                    switch $.cookie('lastSelectedView')
+                        when 'stats' then self.showUserStats()
+                        when 'account' then self.showUserAccount()
+                        else self.showUserConsumption()
+                ,
+                -> 
+                    self.showLoginView() 
+                )
+        else
+            self.showLoginView()
+        $('#blackpage').fadeOut()
+
+    showUserConsumption: ->
+        $.cookie('lastSelectedView','consumption',{expires: 14})
+        this.showView('consumption')
+
+    showUserStats: ->
+        $.cookie('lastSelectedView','stats',{expires: 14})
+        this.showView('stats')
+
+    showUserAccount: ->
+        $.cookie('lastSelectedView','account',{expires: 14})
+        this.showView('account')
+
+    showLoginView: ->
+        this.showView("login")
+
+    # Add capitalize function to String objects
+    String.prototype.capitalize = ->
+        return this.charAt(0).toUpperCase() + this.slice(1)
+
+    # This function enable a nice shortcut to show a view
+    # self.showView('login')
+    # will load the /static/js/view/login.js file dynamically (if needed)
+    # then create an instance of LoginView class named loginView
+    # and finally launch the loginView.show() method
+    showView: (viewName) ->
+        self=this
+        viewObjectName=viewName+"View"
+        viewClassName=viewName.capitalize()+"View"
+        viewFileName=viewName+".js"
+        eval( "if ( typeof(self.#{viewObjectName}) == \"undefined\" ) {
+                $.getScript('/static/js/views/#{viewFileName}',function(){
+                    self.#{viewObjectName} = new #{viewClassName}(mainApplication);
+                    self.#{viewObjectName}.show();
+                });
+            } else {
+                self.#{viewObjectName}.show();
+            }" )
+
+mainApplication = new Application
+
+# after document loaded
+$(document).ready( -> 
+    mainApplication.run() 
+)
--- public/static/js/init.js
+++ public/static/js/init.js
@@ -1,189 +1,152 @@
-var MainApplication = function () {
-    self=this;
-    this.user="";
-    this.password="";
-    this.remember=false;
-
-    this.log = function(msg) {
-        if (typeof console != 'undefined') {
-            if (typeof console.log != 'undefined') {
-                console.log(msg);
-            }
-        }
-    }
-
-    this.save = function (info,value) { 
-        this.log('save("'+info+'","'+value+'")');
-        $.cookie(info,value,{expires: 14}); 
-    }
-    this.forget = function (info) { 
-        this.log('forget("'+info+'")');
-        $.cookie(info,null); 
-    }
-
-    this.setRemember = function(remember) { 
-        if (remember) {
-            this.save('user',this.user);
-            this.save('password',this.password);
-            this.save('remember',true);
+(function() {
+  var Application, mainApplication;
+  Application = (function() {
+    function Application() {}
+    Application.prototype.user = "";
+    Application.prototype.password = "";
+    Application.prototype.remember = false;
+    Application.prototype.log = function(msg) {
+      if ((typeof console != "undefined" && console !== null) && (console.log != null)) {
+        return console.log(msg);
+      }
+    };
+    Application.prototype.save = function(info, value) {
+      this.log("save('" + info + "','" + value + "')");
+      return $.cookie(info, value, {
+        expires: 14
+      });
+    };
+    Application.prototype.forget = function(info) {
+      this.log('forget("' + info + '")');
+      return $.cookie(info, null);
+    };
+    Application.prototype.setRemember = function(remember) {
+      if (remember) {
+        this.save('user', this.user);
+        this.save('password', this.password);
+        return this.save('remember', true);
+      } else {
+        this.forget('user');
+        this.forget('password');
+        return this.forget('remember');
+      }
+    };
+    Application.prototype.setUser = function(user) {
+      this.user = user;
+      if (this.remember) {
+        return this.save('user', this.user);
+      }
+    };
+    Application.prototype.setPassword = function(password) {
+      this.password = password;
+      if (this.remember) {
+        return this.save('password', this.password);
+      }
+    };
+    Application.prototype.retrieveSavedPreferences = function() {
+      var self;
+      self = this;
+      self.remember = $.cookie('remember');
+      self.user = $.cookie('user');
+      if (self.user) {
+        self.password = $.cookie('password');
+        return true;
+      }
+      return false;
+    };
+    Application.prototype.logout = function() {
+      $.cookie('user', null);
+      $.cookie('password', null);
+      $.cookie('remember', null);
+      $.cookie('lastSelectedView', null);
+      return true;
+    };
+    Application.prototype.run_after_dependencies = function(files, tests, action) {
+      var file, self, test;
+      self = this;
+      if (files.length === 0) {
+        return action();
+      } else {
+        file = files.pop();
+        test = tests.pop();
+        eval('o=' + test + ';');
+        if (typeof o === "undefined") {
+          return $.getScript(file, function() {
+            return self.run_after_dependencies(files, tests, action);
+          });
         } else {
-            this.forget('user');
-            this.forget('password');
-            this.forget('remember');
+          return self.run_after_dependencies(files, tests, action);
         }
-    }
-
-    this.setUser = function(user) { 
-        this.user = user; 
-        if (this.remember) {
-            this.save('user',this.user);
-        } 
-    }
-
-    this.setPassword = function(password) { 
-        this.password = password; 
-        if (this.remember) {
-            this.save('password',this.password);
-        } 
-    }
-
-    this.retrieveSavedPreferences = function() {
-        var self=this;
-        self.remember = $.cookie('remember');
-        self.user = $.cookie('user');
-        if (self.user) {
-            self.password = $.cookie('password'); 
-            return true;
-        }
-        return false;
-    }
-
-    this.logout = function () {
-        $.cookie('user',null);
-        $.cookie('password',null);
-        $.cookie('remember',null);
-        $.cookie('lastSelectedView',null);
-        return true; // in order not to disable the link
-    }
-
-    // execute the function action after all files are loaded only if needed
-    //
-    // example of usage:
-    //
-    // files=[]; 
-    // tests=[];
-    //
-    // files.push('/static/js/date.js');
-    // tests.push('Date.prototype.setISO8601');
-    //
-    // files.push('/static/js/flot/jquery.flot.js');
-    // tests.push('$.flot');
-    //
-    // mainApplication.run_after_dependencies( files, tests, 
-    //         function() {
-    //            $('#content').load("/static/html/user_stats.html",
-    //                function(){ self.htmlLoaded(self);});
-    //            });
-    //
-    //
-    this.run_after_dependencies = function( files, tests, action ) {
-        self=this;
-        if ( files.length==0 ) {
-            action();
-        } else {
-            file=files.pop();
-            test=tests.pop();
-            eval('o='+test+';');
-            if ( typeof o == "undefined") {
-                $.getScript(file,function() {
-                    self.run_after_dependencies(files, tests, action);
-                });
-            } else {
-                self.run_after_dependencies(files, tests, action);
-            }
-        }
-    }
-
-    this.connectionSuccessful = function(self, success, failed ) {
-        $.ajax({url: '/users/'+self.user+'.json',
-                data: {l: self.user, p: self.password},
-                success: success,
-                error: failed
-                });
-    }
-
-    this.run = function() {
-        var self=this;
-	    if ( self.retrieveSavedPreferences() ) {
-            self.connectionSuccessful(
-                    self,
-                    function() {
-                        var lastSelectedView=$.cookie('lastSelectedView');
-                        if ( lastSelectedView == 'stats' ) {
-	    	                self.showUserStats();
-                        } else if ( lastSelectedView == 'account' ) {
-	    	                self.showUserAccount();
-                        } else {
-	    	                self.showUserConsumption();
-                        }
-                    },
-                    function() { self.showLoginView() } );
-        } else {
-            self.showLoginView();
-        }
-        $('#blackpage').fadeOut();
-    }
-
-    this.showUserConsumption = function() {
-        $.cookie('lastSelectedView','consumption',{expires: 14});
-        this.showView('consumption');
-    }
-
-    this.showUserStats = function() {
-        $.cookie('lastSelectedView','stats',{expires: 14});
-        this.showView('stats');
-    }
-
-    this.showUserAccount = function() {
-        $.cookie('lastSelectedView','account',{expires: 14});
-        this.showView('account');
-    }
-
-
-    this.showLoginView = function() {
-        this.showView("login");
-    }
-    // Add capitalize function to String objects
+      }
+    };
+    Application.prototype.connectionSuccessful = function(self, success, failed) {
+      return $.ajax({
+        url: '/users/' + self.user + '.json',
+        data: {
+          l: self.user,
+          p: self.password
+        },
+        success: success,
+        error: failed
+      });
+    };
+    Application.prototype.run = function() {
+      var self;
+      self = this;
+      if (self.retrieveSavedPreferences()) {
+        self.connectionSuccessful(self, function() {
+          switch ($.cookie('lastSelectedView')) {
+            case 'stats':
+              return self.showUserStats();
+            case 'account':
+              return self.showUserAccount();
+            default:
+              return self.showUserConsumption();
+          }
+        }, function() {
+          return self.showLoginView();
+        });
+      } else {
+        self.showLoginView();
+      }
+      return $('#blackpage').fadeOut();
+    };
+    Application.prototype.showUserConsumption = function() {
+      $.cookie('lastSelectedView', 'consumption', {
+        expires: 14
+      });
+      return this.showView('consumption');
+    };
+    Application.prototype.showUserStats = function() {
+      $.cookie('lastSelectedView', 'stats', {
+        expires: 14
+      });
+      return this.showView('stats');
+    };
+    Application.prototype.showUserAccount = function() {
+      $.cookie('lastSelectedView', 'account', {
+        expires: 14
+      });
+      return this.showView('account');
+    };
+    Application.prototype.showLoginView = function() {
+      return this.showView("login");
+    };
     String.prototype.capitalize = function() {
-        return this.charAt(0).toUpperCase() + this.slice(1);
-    }
-
-    // This function enable a nice shortcut to show a view
-    // self.showView('login')
-    // will load the /static/js/view/login.js file dynamically (if needed)
-    // then create an instance of LoginView class named loginView
-    // and finally launch the loginView.show() method
-    this.showView = function(viewName) {
-        var self=this;
-        viewObjectName=viewName+"View";
-        viewClassName=viewName.capitalize()+"View";
-        viewFileName=viewName+".js"
-
-        eval( "if ( typeof(self."+viewObjectName+") == \"undefined\" ) {" +
-            "$.getScript('/static/js/views/"+viewFileName+"',function(){" +
-                    "self."+viewObjectName+" = new "+viewClassName+"();" +
-                    "self."+viewObjectName+".show();" +
-                "});" +
-        "} else {" +
-            "self."+viewObjectName+".show();" +
-        "}");
-    }
-}
-
-var mainApplication = new MainApplication();
-
-// after document loaded
-$(document).ready(function(){ 
-    mainApplication.run();
-});
-
+      return this.charAt(0).toUpperCase() + this.slice(1);
+    };
+    Application.prototype.showView = function(viewName) {
+      var self, viewClassName, viewFileName, viewObjectName;
+      self = this;
+      viewObjectName = viewName + "View";
+      viewClassName = viewName.capitalize() + "View";
+      viewFileName = viewName + ".js";
+      return eval("if ( typeof(self." + viewObjectName + ") == \"undefined\" ) {                $.getScript('/static/js/views/" + viewFileName + "',function(){                    self." + viewObjectName + " = new " + viewClassName + "(mainApplication);                    self." + viewObjectName + ".show();                });            } else {                self." + viewObjectName + ".show();            }");
+    };
+    return Application;
+  })();
+  mainApplication = new Application;
+  $(document).ready(function() {
+    return mainApplication.run();
+  });
+}).call(this);
