a01b34cc1fb6b985625ee411c1352b506ad6d8f7
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2011-02-02 21:15:15 +0100

    Shared DB changed its URL

--- global.rb
+++ global.rb
@@ -8,7 +8,7 @@ $static_files_directory='/static'
 # if the env variable DATABASE_URL is set it is the one choosen
 # With this it works seemlessly with heroku
 $db_url = case ENV['GPENV']
-	when "PRODUCTION" then "postgres://mnyqdiisby:f5lxx1zz5pnorynqglhzmsp34@ec2-174-129-199-187.compute-1.amazonaws.com/mnyqdiisby"
+	when "PRODUCTION" then "postgres://ztbtqyozkx:0HfxZXaHzy9s62MFLYbg@ec2-174-129-199-187.compute-1.amazonaws.com/mnyqdiisby"
 	when "PREPROD" then "postgres://xnerjchzzq:f5lxx1zz5pnorynqglhzmsp34@ec2-50-16-228-68.compute-1.amazonaws.com/xnerjchzzq"
 	when "LGP" then "postgres://ozzouhncpp:f5lxx1zz5pnorynqglhzmsp34@ec2-184-72-236-246.compute-1.amazonaws.com/ozzouhncpp"
 	when "Y" then "" # TODO
